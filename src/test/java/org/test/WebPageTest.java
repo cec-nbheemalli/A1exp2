1e471f76d1b49274aaf4f6ad05ae61923ce3fc54
Nitin <nitinsb@cityengineeringcollege.ac.in>
2026-04-08 06:44:00 +0530

    Modified

--- src/test/java/org/test/WebPageTest.java
+++ src/test/java/org/test/WebPageTest.java
@@ -6,21 +6,21 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertTrue;
-class WebpageTest {
+class WebPageTest {
     private static WebDriver driver;
     @BeforeTest
     public void openBrowser() throws InterruptedException {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         Thread.sleep(2000);
-        driver.get("https://cec-nbheemalli.github.io/A1exp2/");
+        driver.get("https://cec-nbheemalli.github.io/lab_2/"); // "Note: You should use your GITHUB-URL here...!!!"
     }
     @Test
     public void titleValidationTest(){
         String actualTitle = driver.getTitle();
         String expectedTitle = "My Simple Website";
         Assert.assertEquals(actualTitle, expectedTitle);
-        assertTrue(true, "Title should contain 'My Simple Website'");
+        assertTrue(true, "Title contains 'simple website'");
     }
     @AfterTest
     public void closeBrowser() throws InterruptedException {
